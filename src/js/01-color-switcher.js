d61a7f41728d8cb087843b03a5529d24d316fd36
Eugene103 <chertyka74@gmail.com>
2023-08-28 22:10:45 +0300

    ack-1`

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector(`[data-start]`)
+const stopBtn = document.querySelector(`[data-stop]`)
+const body = document.body
 
+onPageStart()
+function onPageStart() {
+    stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener(`click`, () => {
+    timerId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+         body.style.backgroundColor = randomColor;
+         
+    }, 1000)
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener(`click`, () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
