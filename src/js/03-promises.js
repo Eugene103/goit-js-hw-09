18236c09a4bf02c690716714027a21bc85f50906
Eugene103 <chertyka74@gmail.com>
2023-08-29 17:40:38 +0300

    ack-3`

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from "notiflix";
+const form = document.querySelector(`.form`)
+const createBtn = document.querySelector(`button`)
+
+createBtn.addEventListener(`click`, eventClick)
+
+function eventClick(evt) {
+  evt.preventDefault();
+  let delay = Number(form.delay.value);
+  const step = Number(form.step.value);
+  const amount = Number(form.amount.value);
+
+  for (let i = 1; i <= amount; i += 1){
+createPromise(i, delay)
+    delay += step
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  })
 }
